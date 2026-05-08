2d1ce896582404d61943709aa948f472669c4e21
tomatyu-kenkyu <tomatyu2008@gmail.com>
2026-05-08 08:50:16 +0000

    e

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,19 @@
-import streamlit as st
 from paddleocr import PaddleOCR
+import streamlit as st
 from PIL import Image
 import numpy as np
 
-st.title("AI OCR アプリ")
+st.title("OCR App")
 
-# OCRモデル読み込み
 @st.cache_resource
 def load_ocr():
     return PaddleOCR(
         use_angle_cls=True,
-        lang='japan'
+        lang='japanese'
     )
 
 ocr = load_ocr()
 
-# 画像アップロード
 uploaded_file = st.file_uploader(
     "画像をアップロード",
     type=["png", "jpg", "jpeg"]
@@ -25,24 +23,15 @@ if uploaded_file:
 
     image = Image.open(uploaded_file)
 
-    st.image(image, caption="アップロード画像")
+    st.image(image)
 
-    img_array = np.array(image)
+    img = np.array(image)
 
-    with st.spinner("OCR実行中..."):
+    result = ocr.ocr(img)
 
-        result = ocr.ocr(img_array)
-
-    extracted_text = ""
+    text = ""
 
     for line in result[0]:
-        text = line[1][0]
-        extracted_text += text + "\n"
+        text += line[1][0] + "\n"
 
-    st.subheader("認識結果")
-
-    st.text_area(
-        "",
-        extracted_text,
-        height=300
-    )
+    st.text_area("OCR結果", text, height=300)
