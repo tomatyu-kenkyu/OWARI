ee84d910552a98f2db6e1aba46060a9adaaa7570
tomatyu-kenkyu <tomatyu2008@gmail.com>
2026-05-08 08:58:47 +0000

    f

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,42 @@
 import streamlit as st
-from paddleocr import PaddleOCR
+import easyocr
+from PIL import Image
+import numpy as np
 
-st.write("読み込み成功")
+st.title("AI OCR")
+
+@st.cache_resource
+def load_reader():
+    return easyocr.Reader(['ja', 'en'])
+
+reader = load_reader()
+
+uploaded_file = st.file_uploader(
+    "画像をアップロード",
+    type=["png", "jpg", "jpeg"]
+)
+
+if uploaded_file:
+
+    image = Image.open(uploaded_file)
+
+    st.image(image, caption="アップロード画像")
+
+    img = np.array(image)
+
+    with st.spinner("OCR実行中..."):
+
+        result = reader.readtext(img)
+
+    text = ""
+
+    for r in result:
+        text += r[1] + "\n"
+
+    st.subheader("OCR結果")
+
+    st.text_area(
+        "",
+        text,
+        height=300
+    )
